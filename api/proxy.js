8be1893ef9fe8d2dc06a13f7bf5ebb83ec4d3690
nxtjdc <50472704+nxtjdc@users.noreply.github.com>
2023-04-25 10:16:28 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://sc.panda321.com/";
+  let target = "https://scholar.lanfanshu.cn/";
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
