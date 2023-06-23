fc8d35a222b25899cdacea63c2330f98daac6f03
nxtjdc <50472704+nxtjdc@users.noreply.github.com>
2023-06-23 23:05:36 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://chat.openai.com/auth/login";
+  let target = "http://jwc.ahu.edu.cn/10314/list.htm";
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
